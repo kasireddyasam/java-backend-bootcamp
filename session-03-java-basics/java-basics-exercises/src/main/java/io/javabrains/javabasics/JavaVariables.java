91ef9a77aa61a3e63d51f1c83dbff2a27fbfd03c
kasireddyasam <kasireddyasam13177@gmail.com>
2025-05-10 22:40:10 +0530

    started the java backend

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
@@ -5,28 +5,38 @@ public class JavaVariables {
     public static void main(String[] args) {
         // Step 1: Declare and initialize variables of different data types
         // int variable to store an integer value
+        int var =123456;
 
         // long variable to store a long value
+        long num = 122343434344543L;
 
         // float variable to store a floating-point value
+        float flt = 123.45637F;
 
 
         // double variable to store a double value
+        double db = 123.456789;
 
 
         // char variable to store a single character
+        char a = 'a';
 
 
         // boolean variable to store a boolean value
+        boolean istrue = true;
 
 
         // String variable to store a string of characters
+        String name = "kasireddy";
 
 
         // Step 2: Print the values of the variables to the console
+        System.out.println(var);
+        System.out.println(name);
 
 
         // Step 3: Modify the values of the variables and print the updated values
+
 
     }
 }
